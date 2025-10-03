8d0825ad2faf2c80ac660cd9b9cce3638323d5d1
Ngoc Han <lengochan090105@gmail.com>
2025-10-03 16:38:09 +0700

    maintenance

---
+++ src/main/java/com/ecar/ecarservice/repositories/MaintenanceItemRepository.java
@@ -0,0 +1,8 @@
+package com.ecar.ecarservice.repositories;
+
+import com.ecar.ecarservice.enitiies.MaintenanceItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MaintenanceItemRepository extends JpaRepository<MaintenanceItem, Long> {}
